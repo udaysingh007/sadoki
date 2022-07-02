4fd3707712c0266443b276bf9bc4d395792651bb
udaysingh007 <usingh@gmail.com>
2022-07-02 12:18:52 -0400

    sms messaging code added

--- main.py
+++ main.py
@@ -2,23 +2,39 @@ import RPi.GPIO as GPIO         # using Rpi.GPIO module
 from time import sleep          # import function sleep for delay
 import logging
 from detection import objectdetection as od
+from temperature import thermalsensor as ts 
+from sms import twilioagent sms
 
 logging.basicConfig(level=logging.DEBUG,
                     format='(%(threadName)-9s) %(message)s',)
+
+to_number = '+19175824874'
+
+def cleanup():
+    ts.cleanup()
+    
     
 if __name__ == '__main__':
     
     logging.debug('Start of main')
     
     retVal = 0
+    temperature = 0
+    humidity = 0
     
     while True:
+        logging.debug('calling ts.read()')
+        ts.readsensor(temperature, humidity)
+        logging.debug('returned from ts.read()')
+
         logging.debug('calling detect_object()')
         retVal = od.detect_object()
         logging.debug('returned from detect_object()')
         
         # if objects found, then break from while
         if (retVal):
+            msgbody = "Sadoki lifeguard alert: Occupants in car with temperature: " + temperature
+            sms.sendsms(msgbody, to_number)
             break
         
         # sleep for 1 minute
@@ -27,7 +43,7 @@ if __name__ == '__main__':
     
     logging.debug('Exiting main')
     
-    #sleep(15);
+    cleanup()
 
 
 
